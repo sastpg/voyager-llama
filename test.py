2374b78bc4c6bcd06504b9d14d2cdc3a0a4a16ce
cshunzi <1754223034@qq.com>
2024-04-02 20:05:27 +0800

    remove openai key

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ env_wait_ticks = 100
 voyager = Voyager(
     mc_port=mc_port,
     env_wait_ticks=env_wait_ticks,
-    openai_api_key="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",
     skill_library_dir="./skill_library"
 )
 
