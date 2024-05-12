4434503a6ca38d130d8789cced84c5e65057865f
cshunzi <1754223034@qq.com>
2024-05-12 15:11:51 +0800

    subgoal test done 2rounds

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from voyager import Voyager
 
-mc_port = 50806
+mc_port = 55861
 env_wait_ticks = 100
 
 voyager = Voyager(
@@ -15,10 +15,11 @@ voyager = Voyager(
 task_z = "1 zombie"
 single_mob_list = ["1 zombie", "1 skeleton", "1 spider", "1 cave_spider", "1 enderman", 
                    "1 blaze", "1 ghast", "1 piglin", "1 piglin_brute", "1 wither_skeleton"]
-test_sub_goals = [["craft crafting table"], ["craft wooden pickaxe"], ["craft stone pickaxe"], ["craft iron pickaxe"], ["mine diamond"]]
-for test_sub_goal in test_sub_goals:
-    voyager.inference_sub_goal(task="subgoal_test", sub_goals=test_sub_goal)
-    voyager.respawn_and_clear()
+test_sub_goals = [["craft wooden pickaxe"], ["craft stone pickaxe"], ["craft iron pickaxe"], ["mine diamond"], ["craft crafting table"]]
+while True:
+    for test_sub_goal in test_sub_goals:
+        voyager.inference_sub_goal(task="subgoal_test", sub_goals=test_sub_goal)
+        voyager.respawn_and_clear()
 for task in single_mob_list:
     voyager.inference(task=task)
 # voyager.learn()
--- test_env/respawnAndClear.js
+++ test_env/respawnAndClear.js
@@ -1,5 +1,5 @@
 async function respawnAndClear(bot) {
-    await bot.chat("/tp @a ~100 ~ ~100");
+    await bot.chat("/tp @a ~100 ~100 ~100");
     await bot.chat("/clear @a");
     await bot.chat("Teleported to new location and reset inventory.");
 }
