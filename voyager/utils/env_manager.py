a05d43ff553a33d73b06e939244a53f7a0994cfc
AKai <2780896955@qq.com>
2024-05-15 15:33:25 +0800

    env SENTENT_EMBEDDINGD_DIR

--- voyager/utils/env_manager.py
+++ voyager/utils/env_manager.py
@@ -4,5 +4,6 @@ class EnvManager:
     def __init__(self):
         self.config = {
             'MC_SERVER_HOST': os.getenv('MC_SERVER_HOST', 'localhost'),
-            'MC_SERVER_PORT': os.getenv('MC_SERVER_PORT', '25565')
+            'MC_SERVER_PORT': os.getenv('MC_SERVER_PORT', '25565'),
+            'SENTENT_EMBEDDING_DIR': os.getenv('SENTENT_EMBEDDING_DIR', '/sentent-embedding')
         }
