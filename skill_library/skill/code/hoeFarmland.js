15039cc1bef28b6dc9f8580f49432d0505e7f45d
cshunzi <1754223034@qq.com>
2024-05-22 20:25:34 +0800

    update benchmark

--- skill_library/skill/code/hoeFarmland.js
+++ skill_library/skill/code/hoeFarmland.js
@@ -1,32 +1,36 @@
 async function hoeFarmland(bot) {
-    await equipHoeOrCraftOne(bot);
-    // find water
-    const water = await exploreUntil(bot, new Vec3(1, 0, 1), 60, () => {
-        const water = bot.findBlocks({
-            matching: block => block.name === "water",
-            maxDistance: 2,
-            count: 1
+    if (await equipHoe(bot)) {
+        // find water
+        const water = await exploreUntil(bot, new Vec3(1, 0, 1), 60, () => {
+            const water = bot.findBlocks({
+                matching: block => block.name === "water",
+                maxDistance: 2,
+                count: 1
+            });
+            return water.length >= 1 ? water : null;
         });
-        return water.length >= 1 ? water : null;
-    });
-    if (!water) {
-        bot.chat("No water nearby.");
-        return;
-    }
-    // find dirt or grass_block near water
-    const dirtNearWater = bot.findBlocks({
-        matching: block => (block.name === "dirt" ||  block.name === "grass_block"),
-        maxDistance: 8,
-        count: 60
-    });
-    // hoe a farmland
-    for (pos of dirtNearWater) {   
-        const farmland = await bot.blockAt(pos);
-        // if there's air above this block and water near this block
-        if (await checkBlockAbove(bot, "air", pos) && await checkBlocksAround(bot, "water", pos)) {
-            await bot.lookAt(pos);
-            await bot.activateBlock(farmland);
-            bot.chat(`hoed block at ${pos}`);
+        if (!water) {
+            bot.chat("No water nearby.");
+            return;
         }
+        // find dirt or grass_block near water
+        const dirtNearWater = bot.findBlocks({
+            matching: block => (block.name === "dirt" ||  block.name === "grass_block"),
+            maxDistance: 8,
+            count: 60
+        });
+        // hoe a farmland
+        for (pos of dirtNearWater) {   
+            const farmland = await bot.blockAt(pos);
+            // if there's air above this block and water near this block
+            if (await checkBlockAbove(bot, "air", pos) && await checkBlocksAround(bot, "water", pos)) {
+                await bot.lookAt(pos);
+                await bot.activateBlock(farmland);
+                bot.chat(`hoed block at ${pos}`);
+            }
+        }
+    } else {
+        bot.chat("No hoe in inventory. Craft a hoe first!");
+        return;
     }
 }
--- test.py
+++ test.py
@@ -19,59 +19,39 @@ voyager = Voyager(
     # embedding_dir="/home/MCagent/paraphrase-multilingual-MiniLM-L12-v2", # linux model path
     environment='combat'
 )
-mob_list = ["1 zombie", "1 skeleton",  "1 blaze", "1 zombified_piglin", "1 wither_skeleton"]
-many_mob_list = [# "5 zombie", 
-                 "1 zombie, 1 skeleton, 1 spider",
-                 "3 skeleton", "5 skeleton"]
-multi_mob_list = [# "1 zombie, 1 skeleton",
-                  # "1 zombie, 1 spider",
-                  "1 zombie, 1 skeleton, 1 spider"]
-farming_task_list = ["plant 1 wheat seed and cook 1 meat (beef / mutton / pork / chicken)",
-                     "collect 1 wool by shears and collect 1 bucket of milk",
-                     "plant 1 melon seed and pumpkin seed",
-                     # "plant 1 wheat seed", "plant 1 melon seed or pumpkin seed", "collect 1 wool by shears or collect 1 bucket of milk", 
-                     # "cook 1 meat (beef / mutton / pork / chicken)", "breed 1 chick", 
-                     # "make 1 bread and cook 1 meat (beef / mutton / pork / chicken)", "make 8 cookies"
-                     ]
+combat_benchmark = [
+                    # Single-mob tasks
+                    "1 zombie", "1 skeleton",  "1 spider", "1 zombified_piglin", "1 enderman",
+                    # Multi-mob tasks
+                    "3 zombie", "5 zombie", "1 zombie, 1 skeleton", "1 zombie, 1 spider", "1 zombie, 1 skeleton, 1 spider"
+                    ]
+farming_benchmark = [
+                    # Single-goal tasks
+                    "hoe a farmland", "breed one chicken", "collect 1 wool by shears or collect 1 bucket of milk",
+                    "cook meat (beef / mutton / pork / chicken)", "plant 1 seed (wheat / melon / pumpkin)",
+                    # Multi-goal tasks
+                    "plant 1 seed (wheat / melon / pumpkin) and cook 1 meat (beef / mutton / pork / chicken)",
+                    "collect 1 wheat and cook 1 meat  (beef / mutton / pork / chicken)",
+                    "collect 1 wool by shears and collect 1 bucket of milk",
+                    "breed 1 sheep and collect 1 wool by shears",
+                    "make cookies"
+                    ]
+# 5 classic MC tasks
 test_sub_goals = ["craft crafting table", "craft wooden pickaxe", "craft stone pickaxe", "craft iron pickaxe", "mine diamond"]
+# skill test
 test_skills = ["collect flowers"]
-farming_sub_goals = [["craft wooden hoe", "collect wheat seed", "hoe farm land", "plant 1 wheat seed", "kill pig", "craft furnace", "mine coal ore", "cook porkchop"],
-                     ["craft wooden hoe", "collect melon", "collect melon seed", "collect pumpkin", "collect pumpkin seed", "hoe farm land", "plant 1 melon seed"],
-                     ["craft a pair of shears", "shear one sheep", "craft bucket", "collect milk"],
-                     ["craft wooden hoe", "collect wheat seed", "hoe farm land", "plant 1 wheat seed"], 
-                     ["craft a pair of shears", "shear one sheep"],
-                     ["craft bucket", "collect milk"],
-                     ["collect wheat seed", "breed chicken"],
-                     ["kill pig", "craft furnace", "mine coal ore", "cook porkchop"],
-                     ["craft wooden hoe", "collect wheat seed", "hoe farm land", "plant 1 wheat seed", "plant 1 wheat seed", "collect wheat", "collect wheat", "breed sheep"],
-                     ["craft wooden hoe", "collect melon", "collect melon seed", "hoe farm land", "plant 1 melon seed"],  
-                     ["craft wooden hoe", "collect pumpkin", "collect pumpkin seed", "hoe farm land", "plant 1 pumpkin seed"]]
 # voyager.learn()
-# for farming_sub_goal in farming_sub_goals:
-#     while True:
-#         try:
-#             voyager.inference_sub_goal(task="subgoal_farming", sub_goals=farming_sub_goal)
-#             break
-#         except Exception as e:
-#             print("error:", e)
+# while True:
+#     voyager.inference_sub_goal(task="skill_test", sub_goals=test_skills)
 while True:
-    voyager.inference_sub_goal(task="skill_test", sub_goals=test_skills)
-while True:
-    for goal in farming_task_list:
+    for goal in farming_benchmark:
         while True:
             try:
                 voyager.learn(goals = goal)
                 break
             except Exception as e:
                 print("error:", e)
-for task in multi_mob_list:
-    while True:
-        try:
-            voyager.inference(task=task)
-            break
-        except Exception as e:
-            print("error:", e)
-for task in many_mob_list:
+for task in combat_benchmark:
     while True:
         try:
             voyager.inference(task=task)
