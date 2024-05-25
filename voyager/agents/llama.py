261cfa729e4ed1237ddc3911805d1f69d2b68903
akai <2780896955@qq.com>
2024-05-25 18:39:13 +0800

    报错可能返回None？

--- voyager/agents/llama.py
+++ voyager/agents/llama.py
@@ -20,6 +20,8 @@ class ModelType:
     LLAMA2_70B = 'llama2_70b'
     LLAMA3_8B_V3 = 'llama3_8b_v3'
     LLAMA3_8B = 'llama3_8b'
+    LLAMA3_70B_V1 = 'llama3_70b_v1'
+
 def call_with_messages(msgs, model_name:ModelType=ModelType.LLAMA3_8B_V3):
     url = f'http://{config.get("server_host")}:{config.get("server_port")}/{model_name}'
     input_msg = {
@@ -28,6 +30,8 @@ def call_with_messages(msgs, model_name:ModelType=ModelType.LLAMA3_8B_V3):
     }
     # print(input_msg)
     result = requests.post(url, json = input_msg)
+    if result is None:
+        return AIMessage(content="")
     json_result = result.json()
     # print(json_result)
     return AIMessage(content=json_result["data"])
