35b395e1fb43322b268c3a17c3efd6ffa9d74095
akai <2780896955@qq.com>
2024-06-04 23:58:50 +0800

    打怪删减任务，增加没微调的8b

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def test_subgoal():
             logger.critical(e)
             traceback.print_exc()
 def test_combat():
-    voyager_l3_8b = Voyager(
+    voyager_l3_8b_v3 = Voyager(
         mc_port=mc_port,
         mc_host=mc_host,
         env_wait_ticks=env_wait_ticks,
@@ -80,6 +80,23 @@ def test_combat():
         curriculum_agent_qa_model_name = ModelType.LLAMA3_8B_V3,
         curriculum_agent_model_name = ModelType.LLAMA3_8B_V3,
         action_agent_model_name = ModelType.LLAMA3_8B_V3,
+    )
+    voyager_l3_8b = Voyager(
+        mc_port=mc_port,
+        mc_host=mc_host,
+        env_wait_ticks=env_wait_ticks,
+        skill_library_dir="./skill_library",
+        reload=True, # set to True if the skill_json updated
+        embedding_dir=embedding_dir, # your model path
+        # embedding_dir="/home/MCagent/paraphrase-multilingual-MiniLM-L12-v2", # linux model path
+        environment='combat',
+        resume=False,
+        server_port=node_port,
+        critic_agent_model_name = ModelType.LLAMA3_8B,
+        comment_agent_model_name = ModelType.LLAMA3_8B,
+        curriculum_agent_qa_model_name = ModelType.LLAMA3_8B,
+        curriculum_agent_model_name = ModelType.LLAMA3_8B,
+        action_agent_model_name = ModelType.LLAMA3_8B,
     )
     voyager_l3_70b = Voyager(
         mc_port=mc_port,
@@ -98,25 +115,38 @@ def test_combat():
         curriculum_agent_model_name = ModelType.LLAMA3_70B_V1,
         action_agent_model_name = ModelType.LLAMA3_70B_V1,
     )
-    combat_benchmark = [
+    
+    multi_rounds_tasks = ["1 enderman", "3 zombie"]
+    l8_v3_combat_benchmark = [
                         # Single-mob tasks
-                        #  "1 skeleton",  "1 spider", "1 zombified_piglin", "1 zombie",
+                        "1 spider", 
                         # Multi-mob tasks
-                        "3 zombie", "1 zombie, 1 skeleton", "1 zombie, 1 spider", "1 zombie, 1 skeleton, 1 spider"
+                        "1 zombie, 1 skeleton", "1 zombie, 1 spider", "1 zombie, 1 skeleton, 1 spider"
                         ]
-    multi_rounds_tasks = ["1 enderman", "5 zombie"]
+    l8_combat_benchmark = [
+                        # Single-mob tasks
+                         "1 skeleton",  "1 spider", "1 zombified_piglin", "1 zombie",
+                        # Multi-mob tasks
+                        "1 zombie, 1 skeleton", "1 zombie, 1 spider", "1 zombie, 1 skeleton, 1 spider"
+                        ]
+    l70_v1_combat_benchmark = [
+                        # Single-mob tasks
+                         "1 skeleton",  "1 spider", "1 zombified_piglin", "1 zombie",
+                        # Multi-mob tasks
+                        "1 zombie, 1 skeleton", "1 zombie, 1 spider", "1 zombie, 1 skeleton, 1 spider"
+                        ]
     MAX_RETRY  = 3
     while True:
         # for task in combat_benchmark:
-        i = 0
         retry = MAX_RETRY
-        while i < len(combat_benchmark):
+        i = 0
+        while i < len(l8_combat_benchmark):
             try:
-                voyager_l3_8b.inference(task=combat_benchmark[i], reset_env=False, feedback_rounds=1)
+                voyager_l3_8b.inference(task=l8_combat_benchmark[i], reset_env=False, feedback_rounds=1)
                 i += 1
                 retry = MAX_RETRY
             except Exception as e:
-                logger.critical(combat_benchmark[i]+' failed. retry...')
+                logger.critical(l8_combat_benchmark[i]+' failed. retry...')
                 logger.critical(e)
                 traceback.print_exc()
                 if retry > 0:
@@ -140,13 +170,13 @@ def test_combat():
                 i += 1
                 retry = MAX_RETRY
         i = 0
-        while i < len(combat_benchmark):
+        while i < len(l70_v1_combat_benchmark):
             try:
-                voyager_l3_70b.inference(task=combat_benchmark[i], reset_env=False, feedback_rounds=1)
+                voyager_l3_70b.inference(task=l70_v1_combat_benchmark[i], reset_env=False, feedback_rounds=1)
                 i += 1
                 retry = MAX_RETRY
             except Exception as e:
-                logger.critical(combat_benchmark[i]+' failed. retry...')
+                logger.critical(l70_v1_combat_benchmark[i]+' failed. retry...')
                 logger.critical(e)
                 traceback.print_exc()
                 if retry > 0:
@@ -158,6 +188,36 @@ def test_combat():
         while i < len(multi_rounds_tasks):
             try:
                 voyager_l3_70b.inference(task=multi_rounds_tasks[i], reset_env=False, feedback_rounds=3)
+                i += 1
+                retry = MAX_RETRY
+            except Exception as e:
+                logger.critical(multi_rounds_tasks[i]+' failed. retry...')
+                logger.critical(e)
+                traceback.print_exc()
+                if retry > 0:
+                    retry -= 1
+                    continue
+                i += 1
+                retry = MAX_RETRY
+        i = 0
+        while i < len(l8_v3_combat_benchmark):
+            try:
+                voyager_l3_8b_v3.inference(task=l8_v3_combat_benchmark[i], reset_env=False, feedback_rounds=1)
+                i += 1
+                retry = MAX_RETRY
+            except Exception as e:
+                logger.critical(l8_v3_combat_benchmark[i]+' failed. retry...')
+                logger.critical(e)
+                traceback.print_exc()
+                if retry > 0:
+                    retry -= 1
+                    continue
+                i += 1
+                retry = MAX_RETRY
+        i = 0
+        while i < len(multi_rounds_tasks):
+            try:
+                voyager_l3_8b_v3.inference(task=multi_rounds_tasks[i], reset_env=False, feedback_rounds=3)
                 i += 1
                 retry = MAX_RETRY
             except Exception as e:
@@ -253,7 +313,6 @@ def test_farming():
                     # Multi-goal tasks
                     "collect and plant 1 seed (wheat or melon or pumpkin)"
                     ]
-    MAX_RETRY = 3
     while True:
         # for task in farming_benchmark:
         i = 0
@@ -261,31 +320,19 @@ def test_farming():
             try:
                 voyager_l3_70b.learn(goals=farming_benchmark[i], reset_env=False)
                 i += 1
-                retry = MAX_RETRY
             except Exception as e:
                 logger.critical(farming_benchmark[i]+' failed. retry...')
                 logger.critical(e)
                 traceback.print_exc() 
-                if retry > 0:
-                    retry -= 1
-                    continue
-                i += 1
-                retry = MAX_RETRY
         i = 0
         while i < len(farming_benchmark):
             try:
                 voyager_l3_8b.learn(goals=farming_benchmark[i], reset_env=False)
                 i += 1
-                retry = MAX_RETRY
             except Exception as e:
                 logger.critical(farming_benchmark[i]+' failed. retry...')
                 logger.critical(e)
                 traceback.print_exc()
-                if retry > 0:
-                    retry -= 1
-                    continue
-                i += 1
-                retry = MAX_RETRY
 
 if __name__ == '__main__':
     test_farming()
