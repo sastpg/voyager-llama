af37569f5f85163deb9f71005b5029d1f619cd47
akai <2780896955@qq.com>
2024-06-13 11:29:16 +0800

    planner 名字

--- odyssey/agents/__init__.py
+++ odyssey/agents/__init__.py
@@ -1,6 +1,6 @@
 from .actor import ActionAgent
 from .critic import CriticAgent
 from .comment import CommentAgent
-from .planer import PlanerAgent
+from .planner import PlanerAgent
 from .skill import SkillManager
 from .llama import call_with_messages
